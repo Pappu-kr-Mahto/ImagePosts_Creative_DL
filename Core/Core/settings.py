c39ed0c01f0cefc6be4607bd3f309add073158a0
Pappu-kr-Mahto <pappukumar2032001@gmail.com>
2024-05-13 15:06:41 +0530

    refactor: Added Hosts name

--- Core/Core/settings.py
+++ Core/Core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(2to0&nu(!axy8-h!hl*m&l7in#avjdgs+j%=*%0^_6%5kge+%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["http://127.0.0.1:8000","http://localhost:8000","https://imageposts-creative.onrender.com/","*"]
 
 
 # Application definition
@@ -156,3 +156,10 @@ SPECTACULAR_SETTINGS = {
     'COMPONENT_SPLIT_REQUEST': True,
     # OTHER SETTINGS
 }
+
+# CORS_ALLOW_ALL_ORIGINS: True
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:3000",
+    "http://localhost:3000",
+    "https://imageposts-creative.onrender.com/"
+]
